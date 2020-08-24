3d9123a862006ec74b451b9fe0d46f8688c307eb
ellowFlower <e01627784@student.tuwien.ac.at>
2020-08-24 19:15:16 +0200

    uncommented code

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 export PYTHONPATH="${PYTHONPATH}:/home/martin/PycharmProjects/bitScan/bitScan"
 
-#python3 ./bitScan/send_getaddr.py &
-python3 ./bitScan/receive_addr.py
-#python3 ./bitScan/send_addr.py
+python3 ./bitScan/send_getaddr.py &
+python3 ./bitScan/receive_addr.py &
+python3 ./bitScan/send_addr.py
